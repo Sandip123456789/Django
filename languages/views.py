aa7f9060732809a4567abe609df88146660fc4b0
sandip chaudhary <sndp.c114@gmail.com>
2020-05-02 09:01:20 +0545

    Learning Django Rest-Framework permissions

--- languages/views.py
+++ languages/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from .models import Language, Paradigm, Programmer
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
 from .serializers import LanguageSerializer, ParadigmSerializer, ProgrammerSerializer
 
 # Create your views here.
 class LanguageList(viewsets.ModelViewSet):
     queryset = Language.objects.all()
     serializer_class = LanguageSerializer
+    #permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
 class ParadigmList(viewsets.ModelViewSet):
     queryset = Paradigm.objects.all()
